8c800cd7838456db476ab2f30a5f3d8419683f1a
Ace <51465755+AceVikings@users.noreply.github.com>
2022-05-23 09:46:59 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -16,6 +16,39 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  solidity: "0.8.7",
+  networks: {
+    matic: {
+      // url: "https://polygon-mumbai.g.alchemy.com/v2/${process.env.INFURA_KEY}",
+      url: "https://rpc-mumbai.maticvigil.com/",
+      
+      accounts: [""]
+    },
+  }
 };
+
+
+// require("@nomiclabs/hardhat-etherscan");
+// require("@nomiclabs/hardhat-waffle");
+// require("hardhat-gas-reporter");
+// require("solidity-coverage");
+// require("@nomiclabs/hardhat-etherscan");
+// require('@openzeppelin/hardhat-upgrades');
+
+// // This is a sample Hardhat task. To learn how to create your own go to
+// // https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// // You need to export an object to set up your config
+// // Go to https://hardhat.org/config/ to learn more
+
